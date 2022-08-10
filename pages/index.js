c822db4900f6eec7496389e055f5b65a7c7067cc
Chris Raible <cmraible1@gmail.com>
2022-08-09 19:42:34 -0700

    adjusting layout slightly, removing favicon

--- pages/index.js
+++ pages/index.js
@@ -77,57 +77,57 @@ export default function Home() {
           </Box>
           <Box>
             <Box pad="xlarge" align="center" justify="center" background="url(friends-3.jpg)" width="100%" style={{minHeight: '100vh'}}>
-              <Box align="center" pad="medium" round background={{color: "black", opacity: 0.7}}>
-                <Paragraph size="large" textAlign="center">What do Facebook, Twitter, Tiktok, Snapchat, Instagram and LinkedIn all have in common?</Paragraph>
-                <Paragraph size="large" textAlign="center">You aren't the customer; you're the product.</Paragraph>
-                <Paragraph size="large" textAlign="center">Their customers are advertisers — not you.</Paragraph>
+              <Box align="start" pad="medium" round background={{color: "black", opacity: 0.7}}>
+                <Paragraph size="large">What do Facebook, Twitter, Tiktok, Snapchat, Instagram and LinkedIn all have in common?</Paragraph>
+                <Paragraph size="large">You aren't the customer; you're the product.</Paragraph>
+                <Paragraph size="large">Their customers are advertisers — not you.</Paragraph>
               </Box>
             </Box>
             
             <Box pad="xlarge" align="center" justify="center" background="url(friends-4.jpg)" width="100%" style={{minHeight: '100vh'}}>
-              <Box align="center" pad="medium" round background={{color: "black", opacity: 0.7}}>
-                <Paragraph size="large" textAlign="center">"But I don't mind seeing an ad every now and then!"</Paragraph>
-                <Paragraph size="large" textAlign="center">The ads aren't the point.</Paragraph>
-                <Paragraph size="large" textAlign="center">It's the incentives the ads create.</Paragraph>
+              <Box align="start" pad="medium" round background={{color: "black", opacity: 0.7}}>
+                <Paragraph size="large">"But I don't mind seeing an ad every now and then!"</Paragraph>
+                <Paragraph size="large">The ads aren't the point.</Paragraph>
+                <Paragraph size="large">It's the incentives the ads create.</Paragraph>
               </Box>
             </Box>
             <Box pad="xlarge" align="center" justify="center" background="url(friends-4.jpg)" width="100%" style={{minHeight: '100vh'}}>
-              <Box align="center" pad="medium" round background={{color: "black", opacity: 0.7}}>
-                <Paragraph size="large" textAlign="center">You see, to target you with ads...</Paragraph>
-                <Paragraph size="large" textAlign="center"><strong>They have to spy on you.</strong></Paragraph>
-                <Paragraph size="large" textAlign="center">And they have to be <strong>addictive</strong>, so you come back.</Paragraph>
-                <Paragraph size="large" textAlign="center">That's why they're always changing —</Paragraph>
-                <Paragraph size="large" textAlign="center">Optimizing for addiction.</Paragraph>
+              <Box align="start" pad="medium" round background={{color: "black", opacity: 0.7}}>
+                <Paragraph size="large">You see, to target you with ads...</Paragraph>
+                <Paragraph size="large"><strong>They have to spy on you.</strong></Paragraph>
+                <Paragraph size="large">And they have to be <strong>addictive</strong>, so you come back.</Paragraph>
+                <Paragraph size="large">That's why they're always changing —</Paragraph>
+                <Paragraph size="large">Optimizing for addiction.</Paragraph>
 
               </Box>
             </Box>
             <Box align="center" pad="xlarge" justify="center" background="url(friends-2.jpg)" width="100%" style={{minHeight: '100vh'}}>
-              <Box align="center" pad="medium" round background={{color: "black", opacity: 0.7}}>
-                <Paragraph size="large" color="white" textAlign="center">Imagine a world...</Paragraph>
-                <Paragraph size="large" color="white" textAlign="center">in which social media companies...</Paragraph>
-                <Paragraph size="large" color="white" textAlign="center">were accountable to their users...</Paragraph>
-                <Paragraph size="large" color="white" textAlign="center">instead of their advertisers.</Paragraph>
+              <Box align="start" pad="medium" round background={{color: "black", opacity: 0.7}}>
+                <Paragraph size="large" color="white">Imagine a world...</Paragraph>
+                <Paragraph size="large" color="white">in which social media companies...</Paragraph>
+                <Paragraph size="large" color="white">were accountable to their users...</Paragraph>
+                <Paragraph size="large" color="white">instead of their advertisers.</Paragraph>
               </Box>
             </Box>
             <Box align="center" pad="xlarge" justify="center" background="url(friends-6.jpg)" width="100%" style={{minHeight: '100vh'}}>
-              <Box align="center" pad="medium" round background={{color: "black", opacity: 0.7}}>
-                <Paragraph size="large" color="white" textAlign="center">Imagine an app that was built for <strong>you</strong>...</Paragraph>
-                <Paragraph size="large" color="white" textAlign="center">and not a bunch of creepy internet marketers.</Paragraph>
+              <Box align="start" pad="medium" round background={{color: "black", opacity: 0.7}}>
+                <Paragraph size="large" color="white">Imagine an app that was built for <strong>you</strong>...</Paragraph>
+                <Paragraph size="large" color="white">and not a bunch of creepy internet marketers.</Paragraph>
               </Box>
             </Box>
             
             <Box pad="xlarge" align="center" justify="center" background="url(friends-5.jpg)" width="100%" style={{minHeight: '100vh'}}>
-              <Box align="center" pad="medium" round background={{color: "black", opacity: 0.7}}>              
-                <Paragraph size="large" textAlign="center">Onlyfrends.lol is a place for you and your friends.</Paragraph>
-                <Paragraph size="large" textAlign="center">It's a place to share ideas, thoughts, pictures, videos, you name it.</Paragraph>
-                <Paragraph size="large" textAlign="center">Everyone is welcome to join onlyfriends for a small monthly fee.</Paragraph>
-                <Paragraph size="large" textAlign="center">Note: You're exchanging money for a service. That makes you a <strong>customer</strong>!</Paragraph>
+              <Box align="start" pad="medium" round background={{color: "black", opacity: 0.7}}>              
+                <Paragraph size="large">Onlyfrends.lol is a place for you and your friends.</Paragraph>
+                <Paragraph size="large">It's a place to share ideas, thoughts, pictures, videos, you name it.</Paragraph>
+                <Paragraph size="large">Everyone is welcome to join onlyfriends for a small monthly fee.</Paragraph>
+                <Paragraph size="large">Note: You're exchanging money for a service. That makes you a <strong>customer</strong>!</Paragraph>
               </Box>
             </Box>
             <Box pad="xlarge" align="center" justify="center" background="url(friends-7.jpg)" width="100%" style={{minHeight: '100vh'}}>
-              <Box align="center" pad="medium" round background={{color: "black", opacity: 0.7}}>              
-                <Paragraph size="large" textAlign="center">We don't care <i>what</i> you're into. We just want you to have a good time here...</Paragraph>
-                <Paragraph size="large" textAlign="center">So you keep paying us money. It's a pretty simple relationship.</Paragraph>
+              <Box align="start" pad="medium" round background={{color: "black", opacity: 0.7}}>              
+                <Paragraph size="large">We don't care <i>what</i> you're into. We just want you to have a good time here...</Paragraph>
+                <Paragraph size="large">So you keep paying us money. It's a pretty simple relationship.</Paragraph>
               </Box>
             </Box>
             <Box pad="xlarge" align="center" justify="center" background="black" width="100%" style={{minHeight: '100vh'}}>
